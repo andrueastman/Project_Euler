35782876658a21b5821b2b552dc71969e4209ce1
Andrew Omondi <andrueastman@gmail.com>
2016-08-25 22:39:20 +0300

    Former approach was really dumb. This works now :).

--- Problem58.java
+++ Problem58.java
@@ -1,92 +1,45 @@
-import java.util.Arrays;
-import java.math.BigInteger;
-
 public class Problem58{
-	enum Direction{
-		RIGHT, UP, LEFT, DOWN;
-	}
- 
-	private static String[][] genUlam(int n){
-		return genUlam(n, 1);
-	}
- 
-	private static String[][] genUlam(int n, int i){
-		String[][] spiral = new String[n][n];
-		Direction dir = Direction.RIGHT;
-		int j = i;
-		int y = n / 2;
-		int x = (n % 2 == 0) ? y - 1 : y; //shift left for even n's
 
-		while(j <= ((n * n) - 1 + i)){
-			spiral[y][x] = isPrime(j) ? String.format("%4d", j) : " ---";
- 
-			switch(dir){
-			case RIGHT:
-				if(x <= (n - 1) && spiral[y - 1][x] == null && j > i) dir = Direction.UP; break;
-			case UP:
-				if(spiral[y][x - 1] == null) dir = Direction.LEFT; break;
-			case LEFT:
-				if(x == 0 || spiral[y + 1][x] == null) dir = Direction.DOWN; break;
-			case DOWN:
-				if(spiral[y][x + 1] == null) dir = Direction.RIGHT; break;
-			}
- 
-			switch(dir){
-				case RIGHT:	x++; break;
-				case UP: 	y--; break;
-				case LEFT:	x--; break;
-				case DOWN:	y++; break;
-			}
-			j++;
-		}
-		return spiral;
-	}
- 
 	public static boolean isPrime(int a){
-		   if(a == 2) return true;
-		   if(a <= 1 || a % 2 == 0) return false;
-		   long max = (long)Math.sqrt(a);
-		   for(long n = 3; n <= max; n += 2){
-		      if(a % n == 0) return false;
-		   }
-		   return true;
+		if(a == 2) 
+			return true;
+		if(a <= 1 || a % 2 == 0) 
+			return false;
+		long max = (long)Math.sqrt(a);
+		for(long n = 3; n <= max; n += 2){
+		    if(a % n == 0) 
+		   		return false;
 		}
+		
+		return true;
+	}
 
-	public static double getPercentage(int n){
-		String[][] ulam = genUlam(n);
-		/*for(String[] row : ulam){
-			System.out.println(Arrays.toString(row).replaceAll(",", ""));
-		}
-		System.out.println();
-		*/
-		int i=0;
-		int primes=0;
-		for(int k=0;k<ulam.length;k++){
-			for(int l=0;l<ulam.length;l++){
-				if(k==l || k+l==ulam.length-1){
-					i++;
-					if(!ulam[k][l].equals(" ---")){
-						primes++;
-					}
-				}
+
+	public static void main(String args[]){
+		System.out.println("Problem 58");
+		int boxsize=1;
+		int num=1;
+		int totalCornerSum=1;
+		int totalPrimeCornerSum=0;
+		for (;;boxsize++) {
+			for (int j=0;j<4 ;j++ ) {
+				num=num+(boxsize*2);
+				//System.out.println(num);
+				totalCornerSum++;
+				if(isPrime(num)){
+					totalPrimeCornerSum++;
+				}	
+			}
+			double ratio= (double)totalPrimeCornerSum*100.0/totalCornerSum;
+			//System.out.println(ratio);
+			if(ratio<10.0){
+				break;	
 			}
 		}
 
-
-		return primes*100.0/i;
-	}
-	public static void main(String[] args){
-		int start=2999;
-		double thresh=10.0;
-		double x=100.0;
-		while(x>thresh){
-			x=getPercentage(start);
-			start=start+2;
-			
-		}
-		System.out.println("Thresh "+x);
-		System.out.println("Number "+start);
-		System.out.println(start);	
-
+		System.out.println("Box Size "+((boxsize*2)+1));
+		
+		System.out.println("Total corner Numbers"+totalCornerSum);
+		System.out.println("Total primes "+totalPrimeCornerSum);
 	}
 }
