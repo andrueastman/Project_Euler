c2534491839d935027f5936689d07d4d1ead0819
Andrew Omondi <andrueastman@gmail.com>
2016-09-11 18:29:08 +0300

    Problem 99 solved!!!

--- Problem99.java
+++ Problem99.java
@@ -7,9 +7,23 @@ public class Problem99{
 	public static void main(String args[]){
 		try{
 			Scanner in = new Scanner(new FileReader("p099_base_exp.txt"));
+			int lineNumber=0;
+			int largestLine=0;
+			double largestResult=0;
 			while(in.hasNextLine()){
-				System.out.println(in.next());
+				String [] pair= in.next().split(",");
+				//System.out.println(pair[0]+":"+pair[1]);
+				double num= Double.parseDouble(pair[0]);
+				double exp= Double.parseDouble(pair[1]);
+				double log=Math.log(num);
+				lineNumber++;
+
+				if((log*exp)>largestResult){
+					largestResult=log*exp;
+					largestLine=lineNumber;
+				}
 			}
+			System.out.println(largestLine);
 		}
 		catch(FileNotFoundException fnfe){
 			fnfe.printStackTrace();
